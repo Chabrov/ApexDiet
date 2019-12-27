516a556bfc95cc0f9100ab7a35a7081aabe08b9e
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2019-12-27 16:29:35 +0100

    trying to fix peer dependencies

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './index.js',
+   entry: './app.js',
    output: {
       path: path.join(__dirname, '/bundle'),
       filename: 'index_bundle.js'
