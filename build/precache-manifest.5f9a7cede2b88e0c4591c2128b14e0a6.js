ac286bf32ff6a3d3af15bd8e5045bc452960525c
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2019-12-29 21:21:19 +0100

    added build

---
+++ build/precache-manifest.5f9a7cede2b88e0c4591c2128b14e0a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a48f8b1c7321623ec394b33b104f75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc897ea66f85237b9642",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "42677f5b86dda9fd2489",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dc897ea66f85237b9642",
+    "url": "./static/js/2.6526288a.chunk.js"
+  },
+  {
+    "revision": "98d7dd25de1cd93b7638ee1e0e3c0d12",
+    "url": "./static/js/2.6526288a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42677f5b86dda9fd2489",
+    "url": "./static/js/main.ab7ba721.chunk.js"
+  },
+  {
+    "revision": "da25df6a3cc10d7e9a13",
+    "url": "./static/js/runtime-main.4c5df372.js"
+  }
+]);
